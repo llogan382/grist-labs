cae525c1f728e03a64863b23fbe08edb1a72d827
Luke <llogan382@gmail.com>
2022-04-01 10:17:21 -0400

    after running build

---
+++ next.config.js
@@ -0,0 +1,24 @@
+module.exports = ({
+  pageExtensions: ["tsx"],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          type: "json",
+          use: "yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+        {
+          test: /\.md$/,
+          loader: "frontmatter-markdown-loader",
+          options: { mode: ["react-component"] },
+        },
+      ]
+    );
+    return config;
+  },
+});
