c38f0b80f2189ed98c16bc0b318b077d6f5fa008
Luke <llogan382@gmail.com>
2022-04-08 09:33:26 -0400

    updates with tutorial

--- next.config.js
+++ next.config.js
@@ -1,25 +1,12 @@
 module.exports = {
-  pageExtensions: ["tsx"],
-  generateBuildId: () => "build-id",
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push(
-      ...[
-        {
-          test: /\.yml$/,
-          type: "json",
-          use: "yaml-loader",
-        },
-        {
-          test: /\.svg$/,
-          use: "@svgr/webpack",
-        },
-        {
-          test: /\.md$/,
-          loader: "frontmatter-markdown-loader",
-          options: { mode: ["react-component"] },
-        },
-      ]
-    );
-    return config;
-  },
-};
+    webpack: (cfg) => {
+        cfg.module.rules.push(
+            {
+                test: /\.md$/,
+                loader: 'frontmatter-markdown-loader',
+                options: { mode: ['react-component'] }
+            }
+        )
+        return cfg;
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
+import { Component } from "react";
+import { attributes, react as HomeContent } from "../content/home.md";
+
+export default function Index({ allPosts }) {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
+      let { title, cats } = attributes;
+  return (
+    <>
+      <Layout>
+        <Head>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
+        <article>
+          <h1>{title}</h1>
+          <HomeContent />
+          <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
